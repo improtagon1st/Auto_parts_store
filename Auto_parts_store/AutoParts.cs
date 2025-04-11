0e607518462aeef364a9cc9703589104ec29363a
improtagon1st <sergei254254254@gmail.com>
2025-04-11 15:11:25 +0300

    Картинки отображаются

--- Auto_parts_store/AutoParts.cs
+++ Auto_parts_store/AutoParts.cs
@@ -29,6 +29,7 @@ namespace Auto_parts_store
         public int StockQuantity { get; set; }
         public string Description { get; set; }
         public Nullable<int> CarModelID { get; set; }
+        public string ImagePath { get; set; }
     
         public virtual Categories Categories { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
